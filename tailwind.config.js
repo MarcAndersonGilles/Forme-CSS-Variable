24de423a41609172b5f11d74d2ed21abbe722736
marc <hornil6748@gmail.com>
2023-10-07 17:57:55 -0400

    test

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,35 @@
 module.exports = {
   content: ["*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "bgBlue": "var(--blue)",
+        
+      },
+      textColor:{
+        "textBlue": "var(--blue)",
+        "textGreen": "var(--green)",
+        "textRed": "var(--red)",
+        "textYellow": "var(--yellow)",
+        "textGray": "var(--gray)",
+        "textBlack": "var(--black)",
+        "textWhite": "var(--white)",
+        skin : {
+          base: "var(--color-text-base)",
+        }
+      }
+    },
   },
-  plugins: [
-    
-  ],
+  variants:{
+    extend: {
+      
+    }
+  },
+  plugins: {
+    'postcss-import': {},
+    'tailwindcss/nesting': {},
+    tailwindcss: {},
+    autoprefixer: {},
+  }
 }
 
